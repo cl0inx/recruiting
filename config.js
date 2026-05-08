2734a891c207bde290f9b55bb1bfb81d422d1f44
cl0inx <colin.torkowski.95@gmail.com>
2026-05-08 20:15:36 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+// Supabase Projekt-URL
+const SUPABASE_URL = 'https://hwajkbdzmeovmqwlhwis.supabase.co';
+
+// Supabase anon/public Key
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh3YWprYmR6bWVvdm1xd2xod2lzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgyNTQ1MTgsImV4cCI6MjA5MzgzMDUxOH0.ctupRzldZUAsyhBJQ1CZkP9jV6fNWjYImikiHQ82E90';
